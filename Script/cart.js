68c11daed659776aa19fd51c9013e9b368c9d86c
Harsh Jain <hj19062001@gmail.com>
2024-02-09 23:37:46 +0530

    first

---
+++ Script/cart.js
@@ -0,0 +1,49 @@
+var choice = JSON.parse(window.sessionStorage.getItem('choice'));
+console.log(choice);
+
+const saveChoice = () => window.sessionStorage.setItem('choice',JSON.stringify(choice));
+
+const inc = x => {
+    choice[x]+=1;
+    addRecord();
+    saveChoice();
+};
+const dec = x => {
+    if (choice[x]!=0) {
+        choice[x]-=1
+        addRecord();
+        saveChoice();
+    }
+};
+const remove = x => {
+    delete choice[x];
+    addRecord();
+    saveChoice();
+};
+
+const addRecord = () => {
+    let record = '', total_amount = 0;
+    for (let k in choice) {
+        record+= `
+            <tr>
+                <td style="text-transform: capitalize;">${booksCollection[k].name}</td>
+                <td>
+                    <button id="b1" onclick='dec(${k})'>-</button>
+                    <span>${choice[k]}</span>
+                    <button id="b2" onclick="inc(${k})">+</button>
+                </td>
+                <td>
+                    ₹<span'>${booksCollection[k].price*choice[k]}</span>
+                    <button id="b3" onclick='remove(${k})'>x</button>
+                </td>
+            </tr>
+        `;
+        total_amount+=booksCollection[k].price*choice[k];
+    }
+    document.getElementById('record').innerHTML = record;
+    document.getElementById('total_amount').innerHTML = total_amount;
+};
+
+window.onload = () => {
+    addRecord();
+};
---
+++ Script/store.js
@@ -0,0 +1,26 @@
+var booksCard='';
+
+var choice = window.sessionStorage.getItem('choice') ? JSON.parse(window.sessionStorage.getItem('choice')) : {};
+
+const listed = x => {
+    document.getElementById('card'+x).classList.toggle('selected');
+    choice[x] ? delete choice[x] : choice[x]=1;
+    saveChoice();
+};
+
+const saveChoice = () => window.sessionStorage.setItem('choice',JSON.stringify(choice));
+
+window.onload = () => {
+    booksCollection.forEach((e,i) => {
+        booksCard+= `
+            <div class="card" id='card${i}' onclick="listed(${i})">
+                <img src=${e.link} />
+                <h2>${e.name}</h2>
+                <h3>Price:- ₹${e.price}</h3>
+            </div><br><br>
+        `;
+    });
+    document.getElementById('books').innerHTML = booksCard;
+    for (let k in choice) 
+        document.getElementById('card'+k).classList.add('selected');
+};
