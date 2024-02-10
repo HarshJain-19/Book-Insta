94c7d2790e2e9a54a636d1c359df3c5352aaa8c1
Harsh Jain <hj19062001@gmail.com>
2024-02-11 01:51:06 +0530

    formatting1

--- Script/cart.js
+++ Script/cart.js
@@ -1,47 +1,51 @@
-var choice = JSON.parse(window.sessionStorage.getItem('choice'));
+var choice = JSON.parse(window.sessionStorage.getItem("choice"));
 console.log(choice);
 
-const saveChoice = () => window.sessionStorage.setItem('choice',JSON.stringify(choice));
+const saveChoice = () =>
+    window.sessionStorage.setItem("choice", JSON.stringify(choice));
 
-const inc = x => {
-    choice[x]+=1;
+const inc = (x) => {
+    choice[x] += 1;
     addRecord();
     saveChoice();
 };
-const dec = x => {
-    if (choice[x]!=0) {
-        choice[x]-=1
+const dec = (x) => {
+    if (choice[x] != 0) {
+        choice[x] -= 1;
         addRecord();
         saveChoice();
     }
 };
-const remove = x => {
+const remove = (x) => {
     delete choice[x];
     addRecord();
     saveChoice();
 };
 
 const addRecord = () => {
-    let record = '', total_amount = 0;
+    let record = "",
+        total_amount = 0;
     for (let k in choice) {
-        record+= `
+        record += `
             <tr>
-                <td style="text-transform: capitalize;">${booksCollection[k].name}</td>
+                <td style="text-transform: capitalize;">${
+                    booksCollection[k].name
+                }</td>
                 <td>
                     <span>${choice[k]}</span><br />
                     <button id="b1" onclick='dec(${k})'>-</button>
                     <button id="b2" onclick="inc(${k})">+</button>
                 </td>
                 <td>
-                    ₹<span>${booksCollection[k].price*choice[k]}</span><br />
+                    ₹<span>${booksCollection[k].price * choice[k]}</span><br />
                     <button id="b3" onclick='remove(${k})'>x</button>
                 </td>
             </tr>
         `;
-        total_amount+=booksCollection[k].price*choice[k];
+        total_amount += booksCollection[k].price * choice[k];
     }
-    document.getElementById('record').innerHTML = record;
-    document.getElementById('total_amount').innerHTML = total_amount;
+    document.getElementById("record").innerHTML = record;
+    document.getElementById("total_amount").innerHTML = total_amount;
 };
 
 window.onload = () => {
--- Script/store.js
+++ Script/store.js
@@ -1,18 +1,21 @@
-var booksCard='';
+var booksCard = "";
 
-var choice = window.sessionStorage.getItem('choice') ? JSON.parse(window.sessionStorage.getItem('choice')) : {};
+var choice = window.sessionStorage.getItem("choice")
+    ? JSON.parse(window.sessionStorage.getItem("choice"))
+    : {};
 
-const listed = x => {
-    document.getElementById('card'+x).classList.toggle('selected');
-    choice[x] ? delete choice[x] : choice[x]=1;
+const listed = (x) => {
+    document.getElementById("card" + x).classList.toggle("selected");
+    choice[x] ? delete choice[x] : (choice[x] = 1);
     saveChoice();
 };
 
-const saveChoice = () => window.sessionStorage.setItem('choice',JSON.stringify(choice));
+const saveChoice = () =>
+    window.sessionStorage.setItem("choice", JSON.stringify(choice));
 
 window.onload = () => {
-    booksCollection.forEach((e,i) => {
-        booksCard+= `
+    booksCollection.forEach((e, i) => {
+        booksCard += `
             <div class="card" id='card${i}' onclick="listed(${i})">
                 <img src=${e.link} />
                 <h2>${e.name}</h2>
@@ -20,7 +23,7 @@ window.onload = () => {
             </div><br><br>
         `;
     });
-    document.getElementById('books').innerHTML = booksCard;
-    for (let k in choice) 
-        document.getElementById('card'+k).classList.add('selected');
+    document.getElementById("books").innerHTML = booksCard;
+    for (let k in choice)
+        document.getElementById("card" + k).classList.add("selected");
 };
